da36bf5cbeaee9ca918f788354a0105b1ec4c548
Teeny12 <93106495+Teeny12@users.noreply.github.com>
2021-11-04 22:01:13 -0400

    up until before the tops

--- src/config.js
+++ src/config.js
@@ -34,7 +34,7 @@ const layerConfigurations = [
       { name: "2" },
       { name: "3" },
       { name: "4" },
-      // { name: "5" },
+      { name: "5" },
       // { name: "6" },
       // { name: "7" },
       // { name: "8" },
