7def5496644c6aa8ec939375c29892504bbc3923
Teeny12 <93106495+Teeny12@users.noreply.github.com>
2021-11-04 22:17:01 -0400

    removed dash from names

--- src/config.js
+++ src/config.js
@@ -35,7 +35,7 @@ const layerConfigurations = [
       { name: "3" },
       { name: "4" },
       { name: "5" },
-      // { name: "6" },
+      { name: "6" },
       // { name: "7" },
       // { name: "8" },
       // { name: "9" },
